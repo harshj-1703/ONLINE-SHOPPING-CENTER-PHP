1c4f10c6d3e59a9899681c661c5beb1644910f41
HARSH JOLAPARA <harshj6680@gmail.com>
2023-07-10 16:40:27 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$keyId = 'rzp_test_MtOk2EPnPUWDN3';
-$keySecret = 'YJJ32cIoDkJyILf5qcWtamiV';
+$keyId = '';
+$keySecret = '';
 $displayCurrency = 'INR';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
